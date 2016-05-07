a6a0c58116fd35ddb7cc7992c6bf9d1e7b6fa7bb
Tate Allen <tatea7@yahoo.com>
2016-05-06 18:44:53 -0700

    Fixed wavetable generation

--- WaveTables/WaveTable.cpp
+++ WaveTables/WaveTable.cpp
@@ -52,7 +52,7 @@ void WaveTable::generateSquareTable()
 
 void WaveTable::generateSawtoothTable()
 {
-	int tableLength = static_cast<int>(floor(SAMPLE_RATE / BASE_FREQUENCY)) + 1;
+	int tableLength = static_cast<int>(floor(SAMPLE_RATE / BASE_FREQUENCY));
 	samples = new double[tableLength];
 	sampleCount = tableLength;
 	for (int i = 0; i < tableLength; ++i) {
